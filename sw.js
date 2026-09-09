f66053230d73ea1f112bacf53c2cda37e9172e10
saulojardim <48941746+saulojardim@users.noreply.github.com>
2026-09-09 13:28:55 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Guarda os arquivos do app no aparelho para abrir mesmo sem internet.
    (Os DADOS do gado ficam no IndexedDB, separado deste cache.) */
 
-const CACHE = "rebanho-v6";
+const CACHE = "rebanho-v7";
 const CORE = ["./", "./index.html", "./manifest.json", "./icon.png", "./boi.png", "./bezerro.png"];
 
 // Instala: baixa e guarda os arquivos essenciais
